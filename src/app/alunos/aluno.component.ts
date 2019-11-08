3c8fcc7becc27318239e1945e43122c1782d1b41
matheusfgomes <matheusfgomes935@gmail.com>
2019-11-08 10:57:54 -0300

    update 2.0

---
+++ src/app/alunos/aluno.component.ts
@@ -0,0 +1,3 @@
+import{Component, OnInit} from '@angular/core';
+import {Aluno} from '../aluno';
+import {Alunos} from '../mock-alunos';
---
+++ src/app/mock-alunos.ts
@@ -0,0 +1,8 @@
+import {Aluno} from './aluno';
+
+
+export const ALUNOS: Aluno[] = [
+  {matricula: 001, nome: 'Sergio Berrantero', curso: 'Gastronomia'}
+  {matricula: 002, nome: 'Ana Beatriz Vasconcelos', curso: 'Astronomia'}
+  {matricula: 003, nome: 'Thiago Fernando Oliveira' , curso: 'Ciência da Computação'}
+]
